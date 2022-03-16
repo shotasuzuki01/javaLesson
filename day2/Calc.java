8a3dcaf8a37129a6122f38887a75e18fc13ce314
shotasuzuki <shotasuzuki01@gmail.com>
2022-03-16 13:19:20 +0900

    Calc.java追加

---
+++ day2/Calc.java
@@ -0,0 +1,20 @@
+public class Calc {
+	public static void main(String[] args){
+		System.out.println("3+3");
+		System.out.println(3-3);//引き算
+		System.out.println(4/3);//割り算
+		System.out.println(4%3);//余りを求める
+		System.out.println(4*3);//掛け算
+		System.out.println(7*(3+2)/3);//11
+		System.out.println(7*3+5/3);//22
+		System.out.println("hello"+"world");//22
+		System.out.println("hello"+"3");//hello3
+		System.out.println(2+"hello"+3);//2hello3
+		//System.out.println(2-"hello"+3);//javaでは文字列に使える演算子は+だけ
+		System.out.println("答えは"+3+2+"です");//答えは32です
+		System.out.println(2.0+3.4);//5.4
+		System.out.println(5.0/2);//2.5
+		System.out.println(5.0+"Hello");//5.0Hello
+		
+}
+}
