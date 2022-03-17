899b4b27c7d5683f8d83bfaf6b57c2cdf8e030c5
shotasuzuki <shotasuzuki01@gmail.com>
2022-03-17 13:16:37 +0900

    乱数の生成

---
+++ day3/Code2_14.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Code2_14{
+	public static void main(String[] args){
+		int r = new java.util.Random().nextInt(90);//0~89の90個
+		//nextInt(6)+5
+		//nextInt
+			System.out.println("あたなはたぶん、" + r + "歳ですね？");
+	}
+}
+
