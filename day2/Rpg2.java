b3c661d31417c6549c438d23298acf3f178b257d
shotasuzuki <shotasuzuki01@gmail.com>
2022-03-17 11:04:36 +0900

    Rpg2

---
+++ day2/Rpg2.java
@@ -0,0 +1,16 @@
+public class Rpg2{
+	public static void main(String[] args){
+		String name="shota";
+		int power = 10;
+		System.out.println("モンスターが2体現れた！");
+		System.out.println("モンスター１を倒した！");
+		System.out.println(name+"は力が10上がった");
+		power = power + 10;
+		System.out.println("モンスター2を倒した！");
+		System.out.println(name+"は力が30上がった");
+		power = power + 30;
+		System.out.println(name+"は40力が上がった");
+
+	}
+}
+
