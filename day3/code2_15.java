287ab10a4bca57e73ebc07adbcae84eab107877b
shotasuzuki <shotasuzuki01@gmail.com>
2022-03-17 13:41:53 +0900

    ユーザーからの入力をうけとる

---
+++ day3/code2_15.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Code2_15{
+	public static void main(String[] args){
+		System.out.println("Name>>");
+		String name = new Scanner(System.in).nextLine();
+			System.out.println("age>>");
+		int age = new Scanner(System.in).nextInt();
+			System.out.println("ようこそ" + age +"歳の"+name + "さん");
+	}
+}
+
