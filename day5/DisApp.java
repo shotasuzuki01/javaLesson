bccdddb1edd08c997a89ad8122d8f6a512a8f8d6
shotasuzuki <shotasuzuki01@gmail.com>
2022-03-23 13:22:31 +0900

    Dice

---
+++ day5/DisApp.java
@@ -0,0 +1,17 @@
+import java.ntil.*;
+public class DisApp{
+	public static void main(String[] args){
+		/*
+		 * さいころを降ってそれが偶数か奇数か判定する
+		  （実行結果）
+			4は偶数です。
+			1は奇数です。
+			*/
+		int dice = new Random().nextInt(6)+1;{
+		if(dice%2==0);{
+		System.out.println("4は偶数です");
+		}else{
+			System.out.println("1は奇数です");
+	}
+}
+
