7cb8d2a83b8286976883835fa5df5f9f25f81425
shotasuzuki <shotasuzuki01@gmail.com>
2022-03-23 10:28:19 +0900

    Ageapp追加

---
+++ day5/AgeApp.java
@@ -0,0 +1,16 @@
+import java .util.*;
+public class AgeApp{
+	public static void main(String[] args){
+		System.out.print("age>>");
+		int age = new Scanner(System.in).nextInt();
+		if(age >= 65){
+			System.out.println("高齢者です");
+		}else if(age >= 20){
+			System.out.println("大人です");
+		}else{
+			System.out.println("子供です");
+		}
+
+	}
+}
+
